e25bf96564e12a3c19cbec8d60ea36a23b74cc3e
ricardo <ricardo@ricardosierra>
2015-05-26 17:09:25 -0300

    Bug na Listagem de Produtos

--- www/mod/comercio/comercio_ProdutoM.php
+++ www/mod/comercio/comercio_ProdutoM.php
@@ -44,23 +44,12 @@ class comercio_ProdutoModelo extends comercio_Modelo
         $perm_editar = $this->_Registro->_Acl->Get_Permissao_Url('comercio/Produto/Produtos_Edit');
         $perm_del = $this->_Registro->_Acl->Get_Permissao_Url('comercio/Produto/Produtos_Del');
         
-        if($perm_editar && $perm_del){
-            $funcao = function( $d, $row ) {
-                return Framework\App\Registro::getInstacia()->_Visual->Tema_Elementos_Btn('Editar'     ,Array('Editar Produto'        ,'comercio/Produto/Produtos_Edit/'.$d.'/'    ,''),true).
-                       Framework\App\Registro::getInstacia()->_Visual->Tema_Elementos_Btn('Deletar'    ,Array('Deletar Produto'       ,'comercio/Produto/Produtos_Del/'.$d.'/'     ,'Deseja realmente deletar essa Produto ?'),true);
-            };
-        }else if($perm_editar){
-            $funcao = function( $d, $row ) {
-                return Framework\App\Registro::getInstacia()->_Visual->Tema_Elementos_Btn('Editar'     ,Array('Editar Produto'        ,'comercio/Produto/Produtos_Edit/'.$d.'/'    ,''),true);
-            };
-        }else if($perm_del){
-            $funcao = function( $d, $row ) {
-                return Framework\App\Registro::getInstacia()->_Visual->Tema_Elementos_Btn('Deletar'    ,Array('Deletar Produto'       ,'comercio/Produto/Produtos_Del/'.$d.'/'     ,'Deseja realmente deletar essa Produto ?'),true);
-            };
-        }else{
-            $funcao = function( $d, $row ) {
-                return '';
-            };
+        $function = '';
+        if($perm_editar){
+            $function .= ' $html .= Framework\App\Registro::getInstacia()->_Visual->Tema_Elementos_Btn(\'Editar\'     ,Array(\'Editar Produto\'        ,\'comercio/Produto/Produtos_Edit/\'.$d.\'/\'    ,\'\'),true);';
+        }
+        if($perm_del){
+            $function .= ' $html .= Framework\App\Registro::getInstacia()->_Visual->Tema_Elementos_Btn(\'Deletar\'    ,Array(\'Deletar Produto\'       ,\'comercio/Produto/Produtos_Del/\'.$d.\'/\'     ,\'Deseja realmente deletar essa Produto ?\'),true);';
         }
 
         
@@ -110,34 +99,19 @@ class comercio_ProdutoModelo extends comercio_Modelo
                        ''.comercio_EstoqueControle::Estoque_Retorna($valor->id); 
                 return $html; 
             });  //'Estoque';
+            if($perm_view)      $function .= ' $html .= $this->_Visual->Tema_Elementos_Btn(\'Visualizar\' ,Array(\'Visualizar Estoque\'    ,\'comercio/Estoque/Estoques/\'.$valor->id.\'/\'    ,\'\'),true);';
+            if($perm_reduzir)   $function .= ' $html .= $this->_Visual->Tema_Elementos_Btn(\'Personalizado\'   ,Array(\'Reduzir Estoque\'  ,\'comercio/Produto/Estoque_Reduzir/\'.$valor->id.\'/\'    ,\'\',\'long-arrow-down\',\'inverse\'),true);';
         }
         if($comercio_Unidade){
             ++$numero;
             $columns[] = array( 'db' => 'unidade2', 'dt' => $numero);  //'Unidade';
         }
-        /*if($comercio_Estoque){
-                    $tabela['Estoque'][$i]   = '<a class="lajax" acao="" href="'.URL_PATH.'comercio/Estoque/Estoques/'.$valor->id.'">'.comercio_EstoqueControle::Estoque_Retorna($valor->id);
-                    if($comercio_Unidade){
-                        $tabela['Estoque'][$i]   .= ' '.$valor->unidade2;
-                    }
-                    $tabela['Estoque'][$i] .= '</a>';
-                    $tabela['Funções'][$i]  = $this->_Visual->Tema_Elementos_Btn('Visualizar' ,Array('Visualizar Estoque'    ,'comercio/Estoque/Estoques/'.$valor->id.'/'    ,''),$perm_view).
-                                            $this->_Visual->Tema_Elementos_Btn('Personalizado'   ,Array('Reduzir Estoque'  ,'comercio/Produto/Estoque_Reduzir/'.$valor->id.'/'    ,'','long-arrow-down','inverse'),$perm_reduzir);
-                }else{
-                    if($comercio_Unidade){
-                        $tabela['Unidade'][$i]   = $valor->unidade2;
-                    }
-                    $tabela['Funções'][$i]   = '';
-                }*/
         
         ++$numero;
+        eval('$function = function( $d, $row ) { $html = \'\'; '.$function.' return $html; };');       
         $columns[] = array( 'db' => 'id',            'dt' => $numero,
-            'formatter' => $funcao
+            'formatter' => $function
         ); //'Funções';
-                /*
-            $function = '$html .= \'<a class="lajax" acao="" href="\'.URL_PATH.\'comercio/Estoque/Estoques/\'.$d.\'">'.
-                    '\'.comercio_EstoqueControle::Estoque_Retorna($valor->id);';
-            eval('$function = function( $d, $row ) { $html = ''; '.$function.' return $html; });');*/
                 
         echo json_encode(
             \Framework\Classes\Datatable::complex( $_GET, Framework\App\Registro::getInstacia()->_Conexao, $tabela, $primaryKey, $columns, null)
